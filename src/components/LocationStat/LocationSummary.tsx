f7e41b83131c08568c2902de2be762129577b43b
flyfish321 <119868298+flyfish321@users.noreply.github.com>
2024-04-17 22:36:19 +0800

    Update LocationSummary.tsx

--- src/components/LocationStat/LocationSummary.tsx
+++ src/components/LocationStat/LocationSummary.tsx
@@ -9,9 +9,9 @@ const LocationSummary = () => {
       <section>
         {years ? <Stat value={`${years.length}`} description=" 年里我走过" /> : null}
         {countries ? <Stat value={countries.length} description="/195 个国家" /> : null}
-        {provinces ? <Stat value={provinces.length} description="/34 个中国省份" /> : null}
+        {provinces ? <Stat value={provinces.length} description="/34 个中国省级行政区" /> : null}
         {cities ? (
-          <Stat value={Object.keys(cities).length} description="/333 个中国城市" />
+          <Stat value={Object.keys(cities).length} description="/333 个中国地级行政区" />
         ) : null}
       </section>
       <hr color="red" />
